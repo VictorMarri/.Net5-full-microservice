332102baf1ae421e68c87f78893a1cc07edbf955
Victor Marri <victor.marri@pottencial.com.br>
2022-09-10 16:08:12 -0300

    PlatformController created (1,53)

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.API.Data;
+using PlatformService.API.DTOs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlatformService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformRepository _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformController(IPlatformRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDTO>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+
+            var platformItems = _repository.GetAllPlatforms();
+            
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDTO>>(platformItems));
+        }
+    }
+}
